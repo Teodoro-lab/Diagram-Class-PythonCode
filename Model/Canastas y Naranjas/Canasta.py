bad056304e8013a350edcc5227768cf4e26ebd00
Teodoro Dawn_CD <66585895+Teodoro-lab@users.noreply.github.com>
2021-01-04 01:12:19 -0600

    Add files via upload

---
+++ Model/Canastas y Naranjas/Canasta.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+#-*- coding: utf-8 -*-
+
+class Canasta:
+    def __init__(self):
+        self.capacidadDeNaranjas = None
+        self.Naranjas = None
+
+    def añadir_Naranja(self, ):
+        pass
+
+    def quitar_Naranja(self, ):
+        pass
+
---
+++ Model/Canastas y Naranjas/Naranja.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+#-*- coding: utf-8 -*-
+
+class Naranja:
+    def __init__(self):
+        self.sabor = None
+        self.color = None
+        self.peso = None
+
+    def hacer_Jugo(self, ):
+        pass
+
